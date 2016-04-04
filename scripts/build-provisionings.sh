6f766f8abde16c5ce0768f9dc1690240fe51103e
Akira Sosa <akirasosa@gmail.com>
2016-04-04 21:32:04 +0700

    pipeline

--- scripts/build-provisionings.sh
+++ scripts/build-provisionings.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
 
+travis login --org --github-token ${GH_TOKEN}
+token=$(travis token --org)
+body='{
+"request": {
+  "branch":"master"
+}}'
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${token}" \
+  -d "${body}" \
+  https://api.travis-ci.org/repo/springboot-angular2-tutorial%2Fmicropost-provisionings/requests
