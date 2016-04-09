ef16c73d5cb9d88446d6e4c94a452bf17771850c
Akira Sosa <akirasosa@gmail.com>
2016-04-09 19:30:01 +0700

    default instance number is 0

--- autoscaling.tf
+++ autoscaling.tf
@@ -17,10 +17,10 @@ resource "aws_launch_configuration" "web" {
 resource "aws_autoscaling_group" "web" {
   name = "web-${var.env}"
   max_size = 4
-  min_size = 1
+  min_size = 0
   health_check_grace_period = 300
   health_check_type = "ELB"
-  desired_capacity = 1
+  desired_capacity = 0
   force_delete = true
   launch_configuration = "${aws_launch_configuration.web.id}"
   vpc_zone_identifier = [
