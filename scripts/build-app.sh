f9e0122241481990b0fc407d265c5831cfba2a8d
Akira Sosa <akirasosa@gmail.com>
2016-04-10 08:26:42 +0700

    build app at initial deploy

---
+++ scripts/build-app.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+travis login --org --skip-completion-check --github-token ${GH_TOKEN}
+token=$(travis token --org)
+body=$(cat << EOS
+{
+  "request": {
+    "branch":"${TRAVIS_BRANCH}"
+  }
+}
+EOS
+)
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${token}" \
+  -d "${body}" \
+  https://api.travis-ci.org/repo/akirasosa%2Ftestapp1/requests
