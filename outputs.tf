2665a971a3309caf9deb4b08fc99a5957864f835
Akira Sosa <akirasosa@gmail.com>
2016-04-13 09:20:48 +0700

    update user data shell

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,3 @@
 output "ami_web" {
   value = "${aws_launch_configuration.web.image_id}"
 }
-
-output "rds_endpoint" {
-  value = "${aws_db_instance.micropost.endpoint}"
-}
-
-// I have only one node..
-output "redis_endpoint" {
-  value = "${aws_elasticache_cluster.micropost.cache_nodes.0.address}:${aws_elasticache_cluster.micropost.cache_nodes.0.port}"
-}
--- templates/web_init.sh
+++ templates/web_init.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Create env file, which will be read from an app.
+ENV_FILE=/opt/micropost/env.sh
+mkdir -p $(dirname ${ENV_FILE})
+cat << EOF > ${ENV_FILE}
+export RDS_ENDPOINT=${rds_endpoint}
+export REDIS_ENDPOINT=${redis_endpoint}
+EOF
+
+# Finalize provisioning by using resolved endpoints.
+(
 cd /opt/provisioning
 
 cat << EOF > inventory
@@ -11,5 +21,7 @@ ansible-playbook -i inventory --connection=local --sudo --diff \
   -e "logstash_elasticsearch_host=${logserver_endpoint}" \
   -e "letsencrypt_host=${web_endpoint}" \
   site.yml
+)
 
+# Update SSL cert
 /usr/local/bin/update_cert.sh >/var/log/update_cert 2>&1
