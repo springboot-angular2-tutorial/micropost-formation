a615209691d00f40c605972762c31c11b06850e5
Akira Sosa <akirasosa@gmail.com>
2016-04-13 08:52:37 +0700

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,19 +3,35 @@
 export ENV=$(echo "${TRAVIS_BRANCH}" | perl -ne "print $& if /(?<=deploy\/).*/")
 if [ -z "${ENV}" ]; then
   echo "Branch name does not match with deploy/xxx. Exit now."
-  exit
+  exit 0
 fi
 
-# Enable Terraform Remote
+echo "Enable Terraform remote."
 scripts/terraform-enable-remote.sh
 
-# Save current output to use it later
+echo "Save current output to use it later"
 terraform output | tee out_before
 
-# Execute Terraform and create tfstate on remote
-scripts/terraform-wrapper.sh plan || exit 0
-echo "test"
+echo "Set ami_web."
+TF_VAR_ami_web=${TF_VAR_ami_web:=$(terraform output ami_web)}
+if [ -z "${TF_VAR_ami_web}" ]; then
+  echo "ami_web is empty. Exit now."
+  exit 0
+fi
+export TF_VAR_ami_web
+echo "ami_web is ${TF_VAR_ami_web} ."
+
+scripts/terraform-wrapper.sh plan
+if [ $? -ne 0 ]; then
+  echo "Plan failed."
+  exit 1
+fi
+
 scripts/terraform-wrapper.sh apply
+if [ $? -ne 0 ]; then
+  echo "Apply failed."
+  exit 1
+fi
 
 # Deploy app when initial deployment
 if [ ! -s out_before ]; then scripts/build-app.sh ; fi
--- scripts/terraform-wrapper.sh
+++ scripts/terraform-wrapper.sh
@@ -2,15 +2,6 @@
 
 . $(dirname $0)/terraform-enable-remote.sh
 
-# If TF_VAR_ami_web is defined already, use it.
-# If TF_VAR_ami_web is not defined, get it from tfstate.
-TF_VAR_ami_web=${TF_VAR_ami_web:=$(terraform output ami_web)}
-if [ -z "${TF_VAR_ami_web}" ]; then
-  echo "ami_web is empty"
-  exit 1
-fi
-export TF_VAR_ami_web
-
 terraform $@ \
   -state="${ENV}.tfstate" \
   -refresh=true \
