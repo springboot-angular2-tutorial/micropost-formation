661e97bc1776b6769b957d05527a6c39d437407a
Akira Sosa <akirasosa@gmail.com>
2016-04-14 21:10:04 +0700

    refactor reading input

--- scripts/export-tfvars.sh
+++ scripts/export-tfvars.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
+# Take over current value, if not new value was specified.
+
 TF_VAR_ami_web=${TF_VAR_ami_web:=$(terraform output ami_web)}
 if [ -n "${TF_VAR_ami_web}" ]; then
   export TF_VAR_ami_web
-else
-  echo "ami_web is required. Exit now."
-  exit 1
 fi
 
 TF_VAR_web_desired_capacity=$(terraform output web_desired_capacity)
