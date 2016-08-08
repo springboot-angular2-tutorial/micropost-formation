795cc6fba36eb54823599d0e8cee68794d2c88f0
Akira Sosa <akirasosa@gmail.com>
2016-08-08 19:02:41 +0700

    deploy in user data shell

--- templates/web_init.sh
+++ templates/web_init.sh
@@ -16,9 +16,11 @@ cd /opt/provisioning
 cat << EOF > inventory
 [web]
 localhost
+[deploy]
+localhost
 EOF
 
-ansible-playbook -i inventory --connection=local --sudo --diff -e "logstash_elasticsearch_host=${logserver_endpoint}" -e "letsencrypt_host=${web_endpoint}"  site.yml
+ansible-playbook -i inventory --connection=local --sudo --diff -e "logstash_elasticsearch_host=${logserver_endpoint}" -e "letsencrypt_host=${web_endpoint}" site.yml
 )
 
 # Update SSL cert
