4b76d0881c1c4b4d9c846dcc5e515b5b119b6b17
Akira Sosa <akirasosa@gmail.com>
2016-08-11 15:39:10 +0700

    evict cert on s3

--- s3.tf
+++ s3.tf
@@ -1,4 +1,13 @@
 resource "aws_s3_bucket" "deploy" {
   bucket = "deploy-${var.env}.hana053.com"
   force_destroy = true
+  // evict letsencrypt cert
+  lifecycle_rule {
+    id = "cert"
+    prefix = "cert/"
+    enabled = true
+    expiration {
+      days = 20
+    }
+  }
 }
--- scripts/export-tfvars.sh
+++ scripts/export-tfvars.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Take over current value, if not new value was specified.
-TF_VAR_web_desired_capacity=$(terraform output web_desired_capacity)
-if [ -n "${TF_VAR_web_desired_capacity}" ]; then
-  export TF_VAR_web_desired_capacity
-fi
-
 if [ "${ENV}" = "prod" ]; then
   # prod is special
   export TF_VAR_web_host_name="micropost"
