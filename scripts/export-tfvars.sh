c8eb335e8461aa3caceb1ccd9767fd80f0ad3ea7
Akira Sosa <akirasosa@gmail.com>
2016-08-19 10:57:24 +0700

    respect current desired capacity

--- scripts/export-tfvars.sh
+++ scripts/export-tfvars.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+asg_name=$(terraform output web_asg_name)
+if [ -n "${asg_name}" ]; then
+  desired_capacity=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name web | jq '.AutoScalingGroups[0].DesiredCapacity')
+  echo "Current desired capacity is ${desired_capacity}."
+  # respect current desired capacity
+  export TF_VAR_web_desired_capacity="${desired_capacity}"
+fi
+
 if [ "${ENV}" = "prod" ]; then
   # prod is special
   export TF_VAR_web_host_name="micropost"
