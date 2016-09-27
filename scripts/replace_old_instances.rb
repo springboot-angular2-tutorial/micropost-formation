c280eec847f2d74c4ba4f58645708377d44b9e8f
Akira Sosa <akirasosa@gmail.com>
2016-09-27 15:55:59 +0700

    use replace_old_instances

--- scripts/replace_old_instances.rb
+++ scripts/replace_old_instances.rb
@@ -9,9 +9,6 @@ ASG_NAME = ARGV[0]
 
 as = Aws::AutoScaling::Client.new
 asg = Aws::AutoScaling::AutoScalingGroup.new(ASG_NAME, client: as)
-elb = Aws::ElasticLoadBalancing::Client.new
-
-old_instances = asg.instances
 
 if asg.desired_capacity * 2 > asg.max_size
   puts "Can not scale out to replace instances in #{ASG_NAME}."
@@ -24,27 +21,4 @@ asg.set_desired_capacity(
   honor_cooldown: true,
 )
 
-# TODO use target group instead of elb
-is_all_in_service = lambda do
-  asg.reload
-  in_services = asg.load_balancers
-                  .map(&:load_balancer_name)
-                  .map { |lb_name| elb.describe_instance_health(load_balancer_name: lb_name).instance_states }
-                  .flatten
-                  .select { |s| s.state == 'InService' }
-  in_services.size >= asg.desired_capacity
-end
-
-puts 'Scaling out to replace old instances.'
-until is_all_in_service.call
-  print '.'
-  sleep 5
-end
-puts ''
-puts 'Completed to scale out.'
-
-puts 'Terminating old instances.'
-old_instances.each do |i|
-  i.terminate(should_decrement_desired_capacity: true)
-end
-puts 'Done.'
+# scaling policy will decrease desired capacity soon.
