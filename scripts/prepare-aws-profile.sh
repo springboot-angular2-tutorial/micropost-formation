6653b441e921462b3b2558d2304ead63072d2c68
Akira Sosa <akirasosa@gmail.com>
2016-11-01 10:20:25 +0700

    mkdir

--- scripts/prepare-aws-profile.sh
+++ scripts/prepare-aws-profile.sh
@@ -2,7 +2,9 @@
 
 set -u
 
-sudo cat << EOS > ${HOME}/.aws/credentials
+mkdir ${HOME}/.aws
+
+cat << EOS > ${HOME}/.aws/credentials
 [micropost-stg]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
@@ -12,7 +14,7 @@ role_arn = ${PROD_ROLE_ARN}
 source_profile = micropost-stg
 EOS
 
-sudo cat << EOS > ${HOME}/.aws/config
+cat << EOS > ${HOME}/.aws/config
 [profile micropost-stg]
 region = ${AWS_DEFAULT_REGION}
 output = json
@@ -22,8 +24,8 @@ region = ${AWS_DEFAULT_REGION}
 output = json
 EOS
 
-sudo chmod 600 ${HOME}/.aws/credentials
-sudo chmod 600 ${HOME}/.aws/config
+chmod 600 ${HOME}/.aws/credentials
+chmod 600 ${HOME}/.aws/config
 
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
